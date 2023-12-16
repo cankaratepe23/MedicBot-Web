b910ec90008b5844c98775959fc36f3b24f46b1f
Can Karatepe <cankaratepe2000@gmail.com>
2023-12-16 22:40:01 +0300

    Switch to functional component

--- src/TagColumn.tsx
+++ src/TagColumn.tsx
@@ -2,29 +2,19 @@ import { ButtonGroup, Button, Typography, Stack, Box } from '@mui/material';
 import React from 'react';
 import { IAudioTrack } from './Interfaces';
 
-interface ITagColumnProps {
-    tagName: string;
-    tracks: IAudioTrack[];
+export default function TagColumn({tagName, tracks}: {tagName: string, tracks: IAudioTrack[]}) {
+    return (
+        <Stack width={250}>
+            <Box p={1} sx={{ bgcolor: 'primary.main', verticalAlign: 'center', textAlign: 'center' }}>
+                <Typography variant='h2'>{tagName}</Typography>
+            </Box>
+            <ButtonGroup orientation='vertical' variant='text'>
+                {tracks.map(track => {
+                    return (
+                        <Button sx={{ bgcolor: 'primary.dark' }} key={track.id}>{track.name}</Button>
+                    );
+                })}
+            </ButtonGroup>
+        </Stack>
+    )
 }
-
-class TagColumn extends React.Component<ITagColumnProps> {
-    render() {
-        console.log(this.props.tagName);
-        return (
-            <Stack width={250}>
-                <Box p={1} sx={{ bgcolor: 'primary.main', verticalAlign: 'center', textAlign: 'center' }}>
-                    <Typography variant='h2'>{this.props.tagName}</Typography>
-                </Box>
-                <ButtonGroup orientation='vertical' variant='text'>
-                    {this.props.tracks.map(track => {
-                        return (
-                            <Button  sx={{ bgcolor: 'primary.dark' }} key={track.name}>{track.name}</Button>
-                        );
-                    })}
-                </ButtonGroup>
-            </Stack>
-        )
-    }
-}
-
-export default TagColumn;
