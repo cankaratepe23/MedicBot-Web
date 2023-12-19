b6752ae219b6d76649161620586fd68de5c7e17d
Can Karatepe <cankaratepe2000@gmail.com>
2023-12-20 02:21:26 +0300

    No tags moved up, add BG image

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { createTheme, CssBaseline, Stack, ThemeProvider, Typography } from '@mui/material';
 import { grey } from '@mui/material/colors';
-import React from 'react';
 import AudioTable from './AudioTable';
+import img11 from './img/11-0-Big-Sur-Color-Night.webp';
+import img12 from './img/12-Dark.webp';
+import img13 from './img/13-Ventura-Dark.webp';
+import img14 from './img/14-Sonoma-Dark.webp';
 
 const theme = createTheme({
   palette: {
@@ -9,7 +12,7 @@ const theme = createTheme({
     primary: {
       main: grey[900],
       light: grey[100],
-      dark: grey[800],
+      dark: '#424242CC',
     },
     text: {
       primary: '#EEE',
@@ -41,10 +44,15 @@ const theme = createTheme({
 });
 
 function App() {
+  const backgrounds = [img11, img12, img13, img14];
+  const bg = backgrounds[Math.floor(Math.random()*backgrounds.length)];
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Stack justifyContent='center' spacing={2} p={2}>
+      <Stack justifyContent='center' spacing={2} p={2} sx={{
+        backgroundImage: `url(${bg})`,
+        backgroundSize: 'contain'
+      }}>
         <Typography variant='h1' align='center'>MedicBot Entries List</Typography>
         <AudioTable />
       </Stack>
--- src/AudioTable.tsx
+++ src/AudioTable.tsx
@@ -6,6 +6,7 @@ import TagColumn from './TagColumn';
 
 export default function AudioTable() {
     const [trackData, setTrackData] = useState<{ [tagName: string]: IAudioTrack[] }>({});
+    const [untaggedTrackData, setUntaggedTrackData] = useState<IAudioTrack[]>([]);
     const [loadComplete, setLoadComplete] = useState(false);
 
     useEffect(() => {
@@ -13,36 +14,53 @@ export default function AudioTable() {
             const response = await fetch(apiUrl + 'Audio');
             const jsonData: IAudioTrack[] = await response.json();
             const grouped: { [tagName: string]: IAudioTrack[] } = {};
+            const nongrouped: IAudioTrack[] = [];
             jsonData.forEach((track) => {
                 let key;
                 if (track.tags && track.tags[0]) {
                     key = track.tags[0];
+                    if (grouped[key]) {
+                        grouped[key].push(track);
+                    }
+                    else {
+                        grouped[key] = [track];
+                    }
                 }
                 else {
-                    key = "!no_tag"
+                    nongrouped.push(track);
                 }
-                if (grouped[key]) {
-                    grouped[key].push(track);
-                }
-                else {
-                    grouped[key] = [track];
-                }
+                
             });
             Object.keys(grouped).forEach((key) => { grouped[key].sort((a, b) => { return a.name.localeCompare(b.name) }) })
             setTrackData(grouped);
+            setUntaggedTrackData(nongrouped.sort((a, b) => { return a.name.localeCompare(b.name) }));
             setLoadComplete(true); // This somehow prevents React from rendering a half-done (only 1 column visible) Masonry view.
         }
         fetchData();
     }, []);
 
+    function splitArrayIntoChunks(array: any[], n: number) {
+        const chunkSize = Math.ceil(array.length / n);
+        const chunks = [];
+        for (let i = 0; i < array.length; i += chunkSize) {
+            chunks.push(array.slice(i, i + chunkSize));
+        }
+        return chunks;
+    }
+
     return (
-        <Masonry sx={{ padding: 2, opacity: loadComplete ? 1 : 0, transition: 'ease-in-out .2s' }} columns={5} spacing={2.5}>
+        <Masonry sx={{ opacity: loadComplete ? 1 : 0, transition: 'ease-in-out .2s' }} columns={5} spacing={2.5}>
             {
-                Object.keys(trackData).sort().map((tagName) => {
+                splitArrayIntoChunks(untaggedTrackData, 5).map((chunk: IAudioTrack[], i: number) => {
+                    return (
+                        <TagColumn key={'!no_tag' + i} tagName='' tracks={chunk} />
+                    )
+                }).concat(
+                Object.keys(trackData).filter((t) => t != '!no_tag').sort().map((tagName) => {
                     return (
                         <TagColumn key={tagName} tagName={tagName} tracks={trackData[tagName]} />
                     );
-                })
+                }))
             }
         </Masonry>
     );
