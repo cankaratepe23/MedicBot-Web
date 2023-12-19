2c7ed8372673ca61df0a545d75dc0d6b12410377
Can Karatepe <cankaratepe2000@gmail.com>
2023-12-19 23:51:46 +0300

    Hide Masonry until loading is done

--- src/AudioTable.tsx
+++ src/AudioTable.tsx
@@ -6,6 +6,7 @@ import TagColumn from './TagColumn';
 
 export default function AudioTable() {
     const [trackData, setTrackData] = useState<{ [tagName: string]: IAudioTrack[] }>({});
+    const [loadComplete, setLoadComplete] = useState(false);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -27,17 +28,19 @@ export default function AudioTable() {
                     grouped[key] = [track];
                 }
             });
+            Object.keys(grouped).forEach((key) => { grouped[key].sort((a, b) => { return a.name.localeCompare(b.name) }) })
             setTrackData(grouped);
+            setLoadComplete(true); // This somehow prevents React from rendering a half-done (only 1 column visible) Masonry view.
         }
         fetchData();
     }, []);
 
     return (
-        <Masonry sx={{ padding: 2 }} columns={5} spacing={2.5}>
+        <Masonry sx={{ padding: 2, opacity: loadComplete ? 1 : 0, transition: 'ease-in-out .2s' }} columns={5} spacing={2.5}>
             {
                 Object.keys(trackData).sort().map((tagName) => {
                     return (
-                        <TagColumn key={tagName} tagName={tagName} tracks={trackData[tagName].sort()} />
+                        <TagColumn key={tagName} tagName={tagName} tracks={trackData[tagName]} />
                     );
                 })
             }
