bbcac6ae4c37a7f2fd22c3836eb5f9d3f0f22b63
Can Karatepe <cankaratepe2000@gmail.com>
2023-12-27 02:47:37 +0300

    Remove ReactDOM use new render

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client';
 import './index.css'
 import App from './App'
 
-ReactDOM.render(
+const rootContainer = document.getElementById('root');
+const root = createRoot(rootContainer!);
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <App />
+</React.StrictMode>
 )
