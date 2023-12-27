3781f3feff266809d7077c390a5e2f0c64e158cf
Can Karatepe <cankaratepe2000@gmail.com>
2023-12-27 03:56:12 +0300

    Add basic search

--- src/AudioTable.tsx
+++ src/AudioTable.tsx
@@ -4,7 +4,37 @@ import { IAudioTrack } from './Interfaces';
 import { apiUrl } from './Properties';
 import TagColumn from './TagColumn';
 
-const AudioTable = memo(function AudioTable({clickCallback}: {clickCallback: (trackId: string) => Promise<void>}) {
+const splitArrayIntoChunks = (array: any[], n: number) => {
+    const chunkSize = Math.ceil(array.length / n);
+    const chunks = [];
+    for (let i = 0; i < array.length; i += chunkSize) {
+        chunks.push(array.slice(i, i + chunkSize));
+    }
+    return chunks;
+}
+
+const filterTrackList = (trackList: IAudioTrack[], query: string) => {
+    if (!query) {
+        return trackList;
+    }
+    return trackList.filter(t => t.name.includes(query) || t.aliases.some(t => t.includes(query)));
+}
+
+const filterTrackData = (trackData: { [tagName: string]: IAudioTrack[] }, query: string) => {
+    if (!query) {
+        return trackData;
+    }
+    const filteredTrackData: { [tagName: string]: IAudioTrack[] } = {};
+    Object.keys(trackData).forEach((tagName) => {
+        const filteredTagGroup = filterTrackList(trackData[tagName], query);
+        if (filteredTagGroup.length != 0) {
+            filteredTrackData[tagName] = filteredTagGroup;
+        }
+    });
+    return filteredTrackData;
+}
+
+const AudioTable = memo(function AudioTable({clickCallback, query}: {clickCallback: (trackId: string) => Promise<void>; query: string}) {
     const [trackData, setTrackData] = useState<{ [tagName: string]: IAudioTrack[] }>({});
     const [untaggedTrackData, setUntaggedTrackData] = useState<IAudioTrack[]>([]);
     const [loadComplete, setLoadComplete] = useState(false);
@@ -39,26 +69,19 @@ const AudioTable = memo(function AudioTable({clickCallback}: {clickCallback: (tr
         fetchData();
     }, []);
 
-    function splitArrayIntoChunks(array: any[], n: number) {
-        const chunkSize = Math.ceil(array.length / n);
-        const chunks = [];
-        for (let i = 0; i < array.length; i += chunkSize) {
-            chunks.push(array.slice(i, i + chunkSize));
-        }
-        return chunks;
-    }
-
+    const filteredUntagged = filterTrackList(untaggedTrackData, query);
+    const filteredTagged = filterTrackData(trackData, query);
     return (
         <Masonry sx={{ opacity: loadComplete ? 1 : 0, transition: 'ease-in-out .2s' }} columns={{ xs: 3, sm: 5 }} spacing={2.5}>
             {
-                splitArrayIntoChunks(untaggedTrackData, 5).map((chunk: IAudioTrack[], i: number) => {
+                splitArrayIntoChunks(filteredUntagged, 5).map((chunk: IAudioTrack[], i: number) => {
                     return (
                         <TagColumn key={'!no_tag' + i} tagName='' tracks={chunk} clickCallback={clickCallback} />
                     )
                 }).concat(
-                Object.keys(trackData).filter((t) => t != '!no_tag').sort().map((tagName) => {
+                Object.keys(filteredTagged).sort().map((tagName) => {
                     return (
-                        <TagColumn key={tagName} tagName={tagName} tracks={trackData[tagName]} clickCallback={clickCallback}/>
+                        <TagColumn key={tagName} tagName={tagName} tracks={filterTrackList(filteredTagged[tagName], query)} clickCallback={clickCallback}/>
                     );
                 }))
             }
